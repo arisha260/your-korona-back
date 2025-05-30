1a9dbfbacc53421c89cdcd1ec946eb55ccb9c5e9
Aleksey Morin <aryoshas@yandex.ru>
2025-05-30 16:04:00 +0300

    changed the withdrawal of orders. I made a filter, endless loading and searching

--- app/Http/Controllers/Order/admin/GetOrderController.php
+++ app/Http/Controllers/Order/admin/GetOrderController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Order\admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\OrderResource;
 use App\Models\Order;
-use App\Services\cache\OrdersService;
+use App\Services\orders\OrdersService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class GetOrderController extends Controller
@@ -19,13 +20,21 @@ class GetOrderController extends Controller
     }
 
 
-    public function __invoke()
+    public function __invoke(Request $request)
     {
 
         Gate::authorize('viewAny', Order::class);
 
-        $orders = $this->orderService->getAll();
+        $orders = $this->orderService->getOrdersByStatus(
+            perPage: $request->input('per_page', 20),
+            page: $request->input('page', 1),
+            sort: $request->input('sort', 'waiting'),
+            search: $request->input('search')
+        );
 
-        return OrderResource::collection($orders);
+        return OrderResource::collection($orders)->additional([
+            'nextPage' => $orders->hasMorePages() ? $orders->currentPage() + 1 : null,
+            'total' => $orders->total(),
+        ]);
     }
 }
